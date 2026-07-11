0fc64f2b97ea01ff19b920662c945181e927ec3c
Waheduzzaman <wahed_zaman@outlook.com>
2026-07-11 15:30:44 +0600

    added conditional black on service page

--- screens/tabs/ServicesScreen.js
+++ screens/tabs/ServicesScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, ScrollView, TouchableOpacity, RefreshControl } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { Ionicons } from '@expo/vector-icons';
+import AppSession from '../../services/AppSession'
 
 const ServiceItem = ({ icon, label, color = "#6b7280" }) => (
   <TouchableOpacity className="items-center justify-center w-[25%] mb-6">
@@ -33,37 +34,68 @@ export default function ServicesScreen() {
   }, []);
 
   return (
-    <ScrollView 
+    <ScrollView
       className="flex-1 bg-gray-50 pt-4"
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }
     >
       {/* Academic Section */}
+      {/* todo:: add Has Any permission  */}
       <ServiceSection title={t('academic')}>
-        <ServiceItem icon="library-outline" label={t('library')} color="#3B82F6" />
-        <ServiceItem icon="calendar-outline" label={t('routine')} color="#10B981" />
+        {AppSession.rights !== 1916 && (
+          <ServiceItem icon="library-outline" label={t('library')} color="#3B82F6" />
+        )}
+
+        {AppSession.rights !== 1912 && (
+          <ServiceItem icon="calendar-outline" label={t('routine')} color="#10B981" />
+        )}
+
+        {/* todo: ask habib bhai */}
         <ServiceItem icon="people-outline" label={t('meeting')} color="#F59E0B" />
-        <ServiceItem icon="exit-outline" label={t('leave')} color="#EF4444" />
-        <ServiceItem icon="document-text-outline" label={t('result')} color="#8B5CF6" />
-        <ServiceItem icon="calendar-outline" label={t('routine')} color="#EC4899" />
-        <ServiceItem icon="people-outline" label={t('meeting')} color="#14B8A6" />
-        <ServiceItem icon="exit-outline" label={t('leave')} color="#6366F1" />
+
+        {AppSession.rights !== 1919 && (
+          <ServiceItem icon="exit-outline" label={t('leave')} color="#EF4444" />
+        )}
+
+        {AppSession.rights !== 1914 && (
+          <ServiceItem icon="document-text-outline" label={t('result')} color="#8B5CF6" />
+        )}
+
       </ServiceSection>
 
       {/* Admission Section */}
       <ServiceSection title={t('admission')}>
-        <ServiceItem icon="cash-outline" label={t('fees')} color="#F59E0B" />
-        <ServiceItem icon="card-outline" label={t('payment')} color="#3B82F6" />
-        <ServiceItem icon="wallet-outline" label={t('wallet')} color="#10B981" />
+
+        {AppSession.rights !== 1915 && (
+          <ServiceItem icon="cash-outline" label={t('fees')} color="#F59E0B" />
+        )}
+
+        {AppSession.rights !== 1915 && (
+          <ServiceItem icon="card-outline" label={t('payment')} color="#3B82F6" />
+        )}
+
+        {AppSession.rights !== 1915 && (
+          <ServiceItem icon="wallet-outline" label={t('wallet')} color="#10B981" />
+        )}
+
+
         <ServiceItem icon="clipboard-outline" label={t('reg_forms')} color="#8B5CF6" />
       </ServiceSection>
 
       {/* Classroom Section */}
       <ServiceSection title={t('classroom')}>
+
         <ServiceItem icon="journal-outline" label={t('diary')} color="#EC4899" />
-        <ServiceItem icon="create-outline" label={t('homework')} color="#14B8A6" />
-        <ServiceItem icon="checkmark-done-outline" label={t('attendance')} color="#EF4444" />
+
+        {AppSession.rights !== 1913 && (
+          <ServiceItem icon="create-outline" label={t('homework')} color="#14B8A6" />
+        )}
+
+        {AppSession.rights !== 1911 && (
+          <ServiceItem icon="checkmark-done-outline" label={t('attendance')} color="#EF4444" />
+        )}
+
       </ServiceSection>
 
       {/* Bottom Padding */}
